5649d589b4e559350030d20d0048a9236d9f9bd3
gs <13231533164@163.com>
2018-08-21 17:05:11 +0800

    rc

--- ketang/oop/oopexeobject1.py
+++ ketang/oop/oopexeobject1.py
@@ -8,3 +8,11 @@ class Game_num():
             num = random.randrange(97,123)
             # 把数字转换成asc码形式
             str_s = chr(num)
+            str_num  = str_num + str_s
+        for i in range(8):
+            num = random.randrange(0,10)
+            str_num = str_num +str(num)
+            return str_num
+
+    def num_game(total,source):
+        while
