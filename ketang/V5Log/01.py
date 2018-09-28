7be7ea94fb9b443aad8724ac0c23beedaeee593b
xiaolinhome <18612463553.163.com>
2018-09-29 01:28:30 +0800

    rc

--- ketang/V5Log/01.py
+++ ketang/V5Log/01.py
@@ -1,10 +1,10 @@
 import logging
 # 自定义日志格式
-LOG_FORMAT = "%{asctime}s======%{levelname}s+++++{message}s"
+LOG_FORMAT = "%(asctime)s=====%(levelname)s++++++%(message)s"
 
 
 # 设置存放日志路径,以及异常显示级别
-logging.basicConfig(filename="xiaolin1.log", level=logging.DEBUG,format=LOG_FORMAT)
+logging.basicConfig(filename="xiaolin2.log", level=logging.DEBUG,format=LOG_FORMAT)
 # format 格式
 # datefmt时间格式
 # level级别
@@ -19,4 +19,3 @@ logging.log(logging.INFO,"INFO")
 logging.log(logging.WARNING,"WARNING")
 logging.log(logging.ERROR,"ERROR")
 logging.log(logging.CRITICAL,"CRITICAL")
-
