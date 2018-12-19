72e8d64f5877ce525a48a1dd0220fab12799123c
v_zhoujialin_dxm <18612463553@163.com>
2018-12-19 15:09:43 +0800

    mysql

---
+++ 数据库/Python-mysql/03select.py
@@ -0,0 +1,22 @@
+import pymysql
+
+db = pymysql.connect(host='localhost',user='root',password='root',db='db_ceshi',port=3306)
+
+cursor = db.cursor()
+cursor.execute('show tables')
+datas = cursor.fetchall()
+
+
+# cursor.excute('select *')
+# datas = cursor.fetchall()
+for data in datas:
+   print(data)
+# cursor.close()
+# except Exception as e:
+#     print("查询失败")
+# 查询所有数据
+'''
+fetchall():接受全部返回结果
+fetchone():获取下一个结果集
+rowcount:只读属性，返回执行语句的影响函数
+'''
