b4cc7827837a1933f452a2ae5036fa64f9949cbb
unknown <zhoujialin@UI.erlinyou.com>
2019-04-29 08:33:26 +0800

    'rc'

--- aut/test4.py
+++ aut/test4.py
@@ -12,14 +12,14 @@ from appium import webdriver
 # }
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
-desired_caps['platformVersion'] = '5.1'
+desired_caps['platformVersion'] = '5.1.1'
 desired_caps['deviceName'] = '127.0.0.1:62001' # baa822b7   a82ccd1d Q8JNNNGUOF8L4PON   127.0.0.1:62001
 desired_caps['app'] = r'D:\apk\boobuz.apk'
 # desired_caps['appPackage'] = 'com.miui.calculator'
 # desired_caps['appActivity'] = 'com.miui.calculator.cal.CalculatorActivity' #/@0xda50b9
 desired_caps['unicodKeyboard'] = 'True'
-desired_caps['resetKeyboard'] ='True'
-
+desired_caps['resetKeyboard'] = 'True'
+time.sleep(5)
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 time.sleep(10)
 
