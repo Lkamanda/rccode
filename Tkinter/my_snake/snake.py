6a47d7a3e5321db47cee8ae7d8db20ca12e03818
zhoujialin <1599200510@qq.com>
2019-06-21 20:58:50 +0800

    'rc'

--- Tkinter/my_snake/snake.py
+++ Tkinter/my_snake/snake.py
@@ -1,4 +1,6 @@
 import random
+
+
 class Food():
     def __init(self):
         ''' 自动产生一个食物 '''
@@ -7,3 +9,4 @@ class Food():
     def new_food(self):
         '''产生一个食物 坐标 随机生成，并且不为蛇当前位置'''
         x = random.randrange()
+        self.postion = x, y  # 存放食物位置
