c1499d3c6616668ad716a691bd0b014eec2e66a4
zhoujialin <1599200510@qq.com>
2019-07-03 18:50:16 +0800

    snake

--- Tkinter/my_snake/mysnake.py
+++ Tkinter/my_snake/mysnake.py
@@ -1,4 +1,7 @@
 import random
+import threading
+import time
+import tkinter
 
 class Food:
     def __init__(self, queue):
@@ -18,13 +21,32 @@ class Food:
         # 此处定义的是dict . k 代表消息类型，v代表此类型的数据
         self.queue.put({"food":self.postion})
 
-class Snake():
+class Snake(threading.Thread):
     """
     蛇的功能：
         1.蛇能动，由我们的上下左右建控制
         2.蛇每次动，都需要重新计算蛇头的位置
         3.检查是否游戏完成的功能
     """
+    def __init__(self, world, queue):
+        # 调用父类多线程的构造函数
+        threading.Thread.__init__(self)
+        self.world = world
+        self.queue
+        self.points_earned = 0  # 游戏分数
+        self.food = Food(self.queue)
+        self.snake_points = [{495, 55}, {485, 55}, {465, 55}, {455, 55}]
+        self.start()
+
+    def run(self):
+        """启动多线程调用次函数， 要求蛇一直跑"""
+        if self.world.is_game_over:
+            self.delete()
+        while not self.world.is_game_over:
+            self.queue.put({"move": self.snake_points})
+            time.sleep(0.5)  # 控制蛇的速度
+            self.move()
+
     def move(self):
         """
         负责蛇的移动
@@ -36,7 +58,7 @@ class Snake():
 
         # 蛇头位置跟实物位置相同时
         if self.food.postion == new_snake_point:
-            self.points_earned + = 1 # 得分加1
+            self.points_earned += 1 # 得分加1
             self.queue.put({"points_earned": self.points_earned})   # 将消息传入消息队列中
             self.food.new_food()  # 就得实物被吃掉，产生新的实物
         # 当蛇的位置和食物不同时
@@ -72,7 +94,23 @@ class Snake():
     def check_game_over(self, snake_point):
         """判断的依据是适合有是否和墙相撞"""
         x,y = snake_point[0], snake_point[1]
-        if not -5< x <
+        if not -5< x < 505 or not -5 < y < 315 or snake_point in self.snake_postions:
+            # 将消息发送至消息队列
+            self.queue.put({'game_over': True})
+
+    def key_pressed(self, e):
+        # keysym 是按键名称
+        self.direction = e.keysym2
+
+
+class world(tkinter):
+    """用来模拟整个画版"""
+    def __init__(self):
+        tkinter.__init__(self)
+        self.queue
+        self.is_game_over = False
+        # 定义画板
+        self.canvas =  Canvas(self, width= 500, height = 300)
 
 
 
