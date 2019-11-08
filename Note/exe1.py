5290855ec9eeff406c43d08c79e3f57f7da2e317
zhoujialin <1599200510@qq.com>
2019-11-08 18:23:48 +0800

    'rc'

--- Note/exe1.py
+++ Note/exe1.py
@@ -1,15 +1,25 @@
-import random
+# import random
+# #
+# #
+# # blue = random.randint(1,22)
+# # print(blue)
+# #
+# # red_list = []
+# # for y in range(1,7):
+# #     x = random.randint(1,33)
+# #     if x in red_list:
+# #         x = random.randint(1,33)
+# #
+# #     red_list.append(x)
+# #
+# # print(red_list)
 
+x = input('请输入数字：')
+a = [3,5]
+#
+# def aa(x, a=a ):
+#     if x in a:
+#         print(x)
+#         print('x存在列表a中')
 
-blue = random.randint(1,22)
-print(blue)
-
-red_list = []
-for y in range(1,7):
-    x = random.randint(1,33)
-    if x in red_list:
-        x = random.randint(1,33)
-
-    red_list.append(x)
-
-print(red_list)
+aa(x=x)
